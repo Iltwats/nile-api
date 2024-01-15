60127f507c8221edfc59a3174df03bd62f9f3cdf
Iltwats <atul.sharma@hackerrank.com>
2024-01-16 00:34:43 +0530

    test for post added

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -1,17 +1,33 @@
 require 'rails_helper'
 
 describe "Books API", type: :request do
-  it 'get status for books' do
-    get '/api/v1/books'
-    expect(response).to have_http_status(:success)
-  end
-  
-  it 'returns all books' do
-    FactoryBot.create(:book, title:"1984", author:"George")
-    FactoryBot.create(:book, title:"1982", author:"Bowel")
-    get '/api/v1/books'
+  describe 'GET /books' do
+    it 'get status for books' do
+      get '/api/v1/books'
+      expect(response).to have_http_status(:success)
+    end
 
-    expect(JSON.parse(response.body).size).to eq(2)
+    it 'returns all books' do
+      FactoryBot.create(:book, title:"1984", author:"George")
+      FactoryBot.create(:book, title:"1982", author:"Bowel")
+      get '/api/v1/books'
+
+      expect(JSON.parse(response.body).size).to eq(2)
+    end
+  end
+
+  describe 'POST /books' do
+    it 'check response code' do
+      post '/api/v1/books', params: { book: {title:'Hello World', author:'Strousoup'} }
+
+      expect(response).to have_http_status(:created)
+    end
+
+    it 'check count' do
+      expect{
+        post '/api/v1/books', params: { book: {title:'Hello World', author:'Strousoup'} }
+    }.to change {Book.count}.from(0).to(1)
+    end
   end
 
 end
