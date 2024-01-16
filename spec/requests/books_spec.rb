a644f2556eed742711c96d08b2b65003d2c0fadc
Iltwats <atul.sharma@hackerrank.com>
2024-01-17 01:04:14 +0530

    rspec modified for new changes

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
 describe "Books API", type: :request do
+  let(:first_author){FactoryBot.create(:author, first_name:'Geroge',last_name:'Michale',age:43)}
+  let(:second_author){FactoryBot.create(:author, first_name:'Barrack',last_name:'Obama',age:23)}
   describe 'GET /books' do
     before do
-      FactoryBot.create(:book, title:"1984", author:"George")
-      FactoryBot.create(:book, title:"1982", author:"Bowel")
+      FactoryBot.create(:book, title:"1984", author:first_author)
+      FactoryBot.create(:book, title:"1982", author:second_author)
     end
     it 'get status for books' do
       get '/api/v1/books'
@@ -14,26 +16,41 @@ describe "Books API", type: :request do
     it 'returns all books' do
       get '/api/v1/books'
 
-      expect(JSON.parse(response.body).size).to eq(2)
+      expect(response_body.size).to eq(2)
     end
   end
 
   describe 'POST /books' do
     it 'check response code' do
-      post '/api/v1/books', params: { book: {title:'Hello World', author:'Strousoup'} }
+      post '/api/v1/books', params: {
+        book: {title:'2 States'},
+        author:{first_name:'Chetan', last_name:'Bhagat', age:45}
+      }
 
       expect(response).to have_http_status(:created)
+      expect(Author.count).to eq(1)
+      expect(response_body).to eq(
+        {
+          'id' => 1,
+          'title' => '2 States',
+          'author_name' => 'Chetan Bhagat',
+          'author_age' => 45
+        }
+      )
     end
 
     it 'check count' do
       expect{
-        post '/api/v1/books', params: { book: {title:'Hello World', author:'Strousoup'} }
-    }.to change {Book.count}.from(0).to(1)
+        post '/api/v1/books', params: {
+          book: {title:'2 States'},
+          author:{first_name:'Chetan', last_name:'Bhagat', age:45}
+        }
+      }.to change {Book.count}.from(0).to(1)
     end
   end
 
   describe 'DELETE /books' do
-    let!(:book) { FactoryBot.create(:book, title:"1984", author:"George") }
+    let!(:book) { FactoryBot.create(:book, title:"1984", author:first_author) }
 
     it 'check destroy status code' do
       delete "/api/v1/books/#{book.id}"
