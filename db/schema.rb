56cd26b304fc205afa593612711d1fce1336bb16
Iltwats <atul.sharma@hackerrank.com>
2024-01-16 21:47:05 +0530

    model associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_16_160809) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_16_161146) do
   create_table "authors", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -21,9 +21,10 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_16_160809) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.string "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "author_id"
+    t.index ["author_id"], name: "index_books_on_author_id"
   end
 
 end
