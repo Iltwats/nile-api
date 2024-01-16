5254c9bea9431ae4f093c15ccfefcbdbb385058e
Iltwats <atul.sharma@hackerrank.com>
2024-01-16 22:43:32 +0530

    controller representers

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -3,7 +3,8 @@ module Api
 
     class BooksController < ApplicationController
       def index
-        render json: Book.all
+        books  = Book.all
+        render json: BooksRepresenter.new(books).as_json
       end
 
       def create
@@ -27,7 +28,7 @@ module Api
       def book_params
         params.require(:book).permit(:title,:author)
       end
-      
+
     end
   end
 end
